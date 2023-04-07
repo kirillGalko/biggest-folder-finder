3bc65e3bccf58ceb3f0e3d0cb5be140c5824f5cb
kirill_galko <kirillg28031983@gmail.com>
2023-04-07 17:24:59 +1100

    Dev

---
+++ src/main/java/Node.java
@@ -0,0 +1,61 @@
+package main.java;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Node {
+    private File folder;
+    private ArrayList<Node> children;
+    private long size;
+    private int level;
+    private long limit;
+
+    public Node (File folder, long limit){
+        this.folder = folder;
+        this.limit = limit;
+        children = new ArrayList<>();
+    }
+
+    public long getLimit(){
+        return limit;
+    }
+
+    public File getFolder(){
+        return folder;
+    }
+
+    public void addChild (Node node){
+        node.setLevel(level + 1);
+        children.add(node);
+    }
+
+    private void setLevel (int level){
+        this.level = level;
+    }
+
+    public ArrayList<Node> getChildren(){
+        return children;
+    }
+
+    public long getSize(){
+        return size;
+    }
+
+    public void setSize (long size){
+        this.size = size;
+    }
+
+    public String toString(){
+        //String size = SizeCalculator.getHumanReadable(getSize());
+        StringBuilder builder = new StringBuilder();
+        String size = SizeCalculator.getHumanReadable(getSize());
+        builder.append(folder.getName() + " - " + size + "\n");
+        for (Node child : children){
+            if (child.getSize() < limit){
+                continue;
+            }
+            builder.append("  ".repeat(level + 1) + child.toString());
+        }
+        return builder.toString();
+    }
+}
---
+++ src/main/java/SizeCalculator.java
@@ -0,0 +1,39 @@
+package main.java;
+
+import java.util.HashMap;
+
+public class SizeCalculator {
+    private static char[] sizeMultipliers = {'B', 'K', 'M', 'G', 'T'};
+    private static HashMap<Character, Integer> char2multiplier = getMultiplier();
+
+    public static String getHumanReadable(long size) {
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+            double value = ((double) size) / Math.pow(1024, i);
+            if (value < 1024) {
+                return Math.round(value * 100)/100. + " " + sizeMultipliers[i] +
+                        (i > 0 ? "b" : "");
+            }
+        }
+        return "Very big!";
+    }
+
+    public static long getSizeFromHumanReadable(String size) {
+
+        char sizeFactor = size
+                .replaceAll("[0-9\\s+]+", "")
+                .charAt(0);
+        int multiplier = char2multiplier.get(sizeFactor);
+        long length = Long.valueOf(size.replaceAll("[^0-9]", ""));
+        long newSize = length * multiplier;
+        return newSize;
+    }
+
+    private static HashMap<Character, Integer> getMultiplier() {
+
+        HashMap<Character, Integer> char2multiplier = new HashMap<>();
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+            char2multiplier.put(sizeMultipliers[i], (int) Math.pow(1024, i));
+        }
+        return char2multiplier;
+    }
+}
